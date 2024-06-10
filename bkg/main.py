3b68f2917b923b69d3f167160218195560f13e6f
Luca Fruzza <lfruzza@hotmail.co.uk>
2024-06-10 21:20:42 +0100

    :alembic: WIP

--- bkg/main.py
+++ bkg/main.py
@@ -36,37 +36,37 @@ def extract_tile(coordinates, image):
         pattern_table = []
         tile_indexes = []"""
 
+
+def derive_coords(tile_number):
+    x = tile_number % 20
+    y = tile_number / 16
+    return (x, y, x + 8, y + 8)
+
+
+def tile_reducer(image, tile_number, pattern_indexes, pattern_table):
+    tile = extract_tile(derive_coords(tile_number), image)
+    existing_index = pattern_table.get(tile)
+    if existing_index:
+        pattern_indexes.append(existing_index)
+    else:
+        index = len(pattern_table)
+        pattern_table[tile] = len(pattern_table)
+        pattern_indexes.append(index)
+
 for infile in sys.argv[1:]:
     try:
         # Open image and confirm correct shape
         im = Image.open(infile)
         width, height = im.size
 
-        if width%8==0 and height%8==0:
-            
-            for y in range(0, height//8):    
-                for x in range(0, width//8):
-                    
-                    starting_coord = [0,0,8,8]
-                    x_increment = [8,0,8,0]
-                    y_increment = [0,8,0,8]
-                    
-                    coords = []
+        pattern_indexes = []
+        pattern_table = {}
 
-                    for i in range(0,4):
-                        coords.append(starting_coord[i]+(x*x_increment[i])+(y*y_increment[i]))
-                    
-                    crop_coords = tuple(coords)
-                    print(crop_coords)
+        for tile_number in range(320):
+            tile_reducer(im, tile_number, pattern_indexes, pattern_table)
 
-                    tile = extract_tile((crop_coords), im)
-                    all_bytes = map(format_byte_string, tile.to_gb_bytes())
-
-                    print(all_bytes)
-                    print('\n')
-
-        else:
-            print(infile, 'not correct dimensions.')
+        print(pattern_indexes)
+        print(pattern_table)
     
     except OSError:
         pass
